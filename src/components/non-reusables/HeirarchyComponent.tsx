4ec5439f52424e15a74001b00da8e45cf8af8962
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-03-03 16:52:40 +0100

    feature: added the account feature update three

--- src/components/non-reusables/HeirarchyComponent.tsx
+++ src/components/non-reusables/HeirarchyComponent.tsx
@@ -1,9 +1,20 @@
-
+import Button from "../reusables/formcomponent/Button";
 
 export const HeirarchyComponent = () => {
   return (
-    <div className="w-full h-[100%]">
-      HeirarchyComponent  
+    <div className="w-full h-[100%] p-4 overflow-auto">
+      <div className="w-full flex justify-between mb-2">
+        <div className="font-bold text-lg">Members Count ({12})</div>
+        <div>
+          <Button
+            buttonLabel="Create Relationship"
+            className="border p-2 
+          rounded-md text-white bg-cyan-400"
+          />
+        </div>
+      </div>
+      <hr />
+
     </div>
   );
 }
