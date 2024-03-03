a5ec5833b424238cd621a100d3ee15a194944478
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-03-03 17:24:10 +0100

    feature: added the account feature update four

--- src/views/PageNotFoundView.tsx
+++ src/views/PageNotFoundView.tsx
@@ -3,6 +3,17 @@
 export const PageNotFoundView = () => {
   
   return (
-    <div>PageNotFoundView</div>
-  )
+    <div className="w-full h-screen">
+      <p className="text-center text-[50px] font-bold text-pink-400">
+        Sorry page not found
+      </p>
+      <div className="h-[100%] flex justify-center items-center">
+        <div className="text-[100px] animate-ping">
+          <span className="text-cyan-400">4</span>
+          <span className="text-pink-400">0</span>
+          <span className="text-cyan-400">4</span>
+        </div>
+      </div>
+    </div>
+  );
 }
